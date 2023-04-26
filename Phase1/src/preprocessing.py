60ecc8d7c6bbf1c64f3fad4bb731162a648682c4
Alireza Ghafari <areza.ghafari01@gmail.com>
2023-04-26 16:23:31 +0330

    Make stopwords list

--- Phase1/src/preprocessing.py
+++ Phase1/src/preprocessing.py
@@ -12,6 +12,12 @@ def extract_contents(my_json):
     res = [documents[i]['content'] for i in my_json]
     return res
 
+def persian_stopwords(path):
+    with open(path) as my_file:
+        list_ = my_file.read().splitlines()
+    return list_
 
 documents = read_file('../data/IR_data_news_12k.json')
 contents = extract_contents(documents)
+stopwords_list = persian_stopwords("../data/stopwords.txt")
+stopwords_set = {stopwords_list[i] for i in range(0, len(stopwords_list))}
