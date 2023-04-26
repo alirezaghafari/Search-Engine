6dded81eb7881c3d96cda124be51d49b03c072f0
Alireza Ghafari <areza.ghafari01@gmail.com>
2023-04-26 16:26:01 +0330

    Normalize words

--- Phase1/src/preprocessing.py
+++ Phase1/src/preprocessing.py
@@ -12,12 +12,28 @@ def extract_contents(my_json):
     res = [documents[i]['content'] for i in my_json]
     return res
 
+
 def persian_stopwords(path):
     with open(path) as my_file:
         list_ = my_file.read().splitlines()
     return list_
 
+
+my_normalizer = Normalizer()
+
+
+def preprocess(contents_):
+    preprocessed_docs = []
+
+    for content in contents_:
+        # normalizing
+        normalized = my_normalizer.normalize(content)
+
+    return preprocessed_docs
+
+
 documents = read_file('../data/IR_data_news_12k.json')
 contents = extract_contents(documents)
 stopwords_list = persian_stopwords("../data/stopwords.txt")
 stopwords_set = {stopwords_list[i] for i in range(0, len(stopwords_list))}
+preprocessed_docs = preprocess(contents)
