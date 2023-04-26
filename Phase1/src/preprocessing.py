3556e52cf940a8e089d0e590e3fa7f86943e2760
Alireza Ghafari <areza.ghafari01@gmail.com>
2023-04-26 16:21:53 +0330

    Extract json file contents

--- Phase1/src/preprocessing.py
+++ Phase1/src/preprocessing.py
@@ -1,11 +1,17 @@
 import json
 from parsivar import Normalizer, Tokenizer, FindStems
 
+
 def read_file(path):
     file = open(path)
     data = json.load(file)
     return data
 
 
+def extract_contents(my_json):
+    res = [documents[i]['content'] for i in my_json]
+    return res
+
 
 documents = read_file('../data/IR_data_news_12k.json')
+contents = extract_contents(documents)
