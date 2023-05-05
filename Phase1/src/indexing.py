662f594ea06bb6f10340046ba661e8c2a663f4d3
Alireza Ghafari <areza.ghafari01@gmail.com>
2023-05-05 16:43:59 +0330

    Start indexing phase

---
+++ Phase1/src/indexing.py
@@ -0,0 +1,9 @@
+from preprocessing import get_tokens
+
+
+def indexing(dictionary):
+    terms_map  =  {}
+    return terms_map
+
+terms_dictionary  =  get_tokens()
+positional_index  =  indexing(terms_dictionary)
