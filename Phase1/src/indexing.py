ca3fd4709581bebf8563582db1e2d01a891bb8a5
Alireza Ghafari <areza.ghafari01@gmail.com>
2023-05-05 16:44:34 +0330

    Define Term class

--- Phase1/src/indexing.py
+++ Phase1/src/indexing.py
@@ -1,6 +1,20 @@
 from preprocessing import get_tokens
 
+class Term:
+    def __init__(self, number_of_docs):
+        self.freq_in_doc  =  {i: 0 for i in range(number_of_docs)}
+        self.pos  =  {}
+        self.freq_in_all_docs  =  0
 
+
+    def posting(self, position, doc_id):
+        if doc_id not in self.pos:
+            self.pos[doc_id] = []
+        self.freq_in_all_docs += 1
+        self.freq_in_doc[doc_id] += 1
+        self.pos[doc_id].append(position)
+
+        
 def indexing(dictionary):
     terms_map  =  {}
     return terms_map
