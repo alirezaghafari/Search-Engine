d8916e891b8249ff9c105fe15c1cb6cfd5362c0a
Alireza Ghafari <areza.ghafari01@gmail.com>
2023-06-09 02:30:03 +0330

    Scoring using tfidf

--- Phase2/src/indexing.py
+++ Phase2/src/indexing.py
@@ -33,3 +33,51 @@ class Term:
 
     def get_champ_list(self):
         return self.champ_list
+
+
+def indexing(dictionary):
+    terms_map = {}
+    for doc_id in range(len(dictionary)):
+        for pos in range(len(dictionary[doc_id])):
+            term = dictionary[doc_id][pos]
+            if term in terms_map:
+                term_obj = terms_map[term]
+            else:
+                term_obj = Term()
+            term_obj.posting(pos, doc_id)
+            terms_map[term] = term_obj
+
+    return terms_map
+
+
+def calculate_tf(term, doc_id):
+    freq = term.freq_in_doc[doc_id]
+    if freq > 0:
+        return 1 + math.log10(freq)
+    return 0
+
+
+def calculate_idf(term, number_of_docs):
+    n = len(term.freq_in_doc)
+    return math.log10(number_of_docs/n)
+
+
+def tfidf_score_function(term, doc_id, number_of_docs):
+    return calculate_tf(term, doc_id) * calculate_idf(term, number_of_docs)
+
+
+def calculate_weights(dictionary, number_of_docs):
+    for term in dictionary:
+        postings_list = dictionary[term].get_docs()
+        for doc_id in postings_list:
+            dictionary[term].set_weight(doc_id, number_of_docs)
+
+
+def calculate_tf_for_query(term, tokens):
+    freq = 0
+    for word in tokens:
+        if word == term:
+            freq += 1
+    if freq > 0:
+        return 1 + math.log10(freq)
+    return 0
